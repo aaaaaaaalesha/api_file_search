e5acaa2b0b5db6e22770cf9edc1a3a4bd8abf5b1
Aleksey Aleksandrov <55093100+aaaaaaaalesha@users.noreply.github.com>
2023-04-19 23:03:06 +0300

    base view logic

--- api_file_searcher/search/models.py
+++ api_file_searcher/search/models.py
@@ -2,41 +2,8 @@ from django.db import models
 
 
 class Search(models.Model):
-    class Operators(models.TextChoices):
-        EQ = 'eq'
-        GR = 'gt'
-        LT = 'lt'
-        GE = 'ge'
-        LE = 'le'
-
     search_id = models.UUIDField(
         primary_key=True,
-    )
-    text = models.CharField(
-        max_length=255,
-        null=True,
-    )
-    file_mask = models.CharField(
-        max_length=255,
-        null=True,
-    )
-    # Size.
-    size_value = models.PositiveIntegerField(
-        null=True,
-    )
-    size_operator = models.CharField(
-        max_length=2,
-        null=True,
-        choices=Operators.choices,
-    )
-    # Creation time.
-    creation_time_value = models.DateTimeField(
-        null=True,
-    )
-    creation_time_operator = models.CharField(
-        max_length=2,
-        null=True,
-        choices=Operators.choices,
     )
     paths = models.TextField(
         default='[]',
--- api_file_searcher/search/views.py
+++ api_file_searcher/search/views.py
@@ -1,12 +1,12 @@
+import json
 import uuid
 
-from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.decorators import action
-from rest_framework.utils.serializer_helpers import ReturnDict
+from django.core.exceptions import ObjectDoesNotExist
 
 from .models import Search
 from .serializers import SearchSerializer
@@ -17,32 +17,46 @@ from .utils import search
 class SearchView(APIView):
     def post(self, request: Request) -> Response:
         serializer = SearchSerializer(data=request.data)
-        if serializer.is_valid():
-            found_paths = search(
-                serializer.data.get('text'),
-                serializer.data.get('filemask'),
-                serializer.data.get('size'),
-                serializer.data.get('creation time'),
+        if not serializer.is_valid():
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST,
             )
-            uuid.uuid4()
 
-            # return Response(
-            #     {'search_id': str()},
-            #     status=status.HTTP_201_CREATED,
-            # )
+        found_paths = search(
+            serializer.data.get('text'),
+            serializer.data.get('filemask'),
+            serializer.data.get('size'),
+            serializer.data.get('creation time'),
+        )
+
+        search_id = uuid.uuid1()
+        Search.objects.create(
+            search_id=search_id,
+            paths=json.dumps(found_paths),
+        )
 
         return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST,
+            {'search_id': str(search_id)},
+            status=status.HTTP_201_CREATED,
         )
 
 
 @action(detail=False, methods=['get'])
 class SearchResultView(APIView):
-    def get(self, request: Request, search_id: str):
-        search = get_object_or_404(Search, search_id=search_id)
-        if search.is_finished:
-            serializer = SearchResultSerializer(search.results, many=True)
-            return Response({'results': serializer.data})
+    def get(self, request: Request, search_id: str) -> Response:
+        try:
+            search_result = Search.objects.get(search_id=search_id)
+        except ObjectDoesNotExist:
+            return Response(
+                {'finished': False},
+                status=status.HTTP_200_OK,
+            )
 
-        return Response({'status': 'search is not finished yet'})
+        return Response(
+            {
+                'finished': True,
+                'paths': list(search_result.paths),
+            },
+            status=status.HTTP_200_OK,
+        )
