f6999fe43b7f98731a9ebc6052763602b6c81c56
aaaaaaaalesha <sks2311211@mail.ru>
2023-04-19 06:47:19 +0300

    search signature

--- api_file_searcher/search/views.py
+++ api_file_searcher/search/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -5,8 +7,32 @@ from rest_framework import status
 from .models import Search, SearchResult
 from .serializers import SearchSerializer, SearchResultSerializer
 
+from typing import Optional, List, Tuple
+
+# TODO: maybe move it to another place
+SEARCH_DIR = os.getenv('SEARCH_DIR', default='.')
+
 
 class SearchView(APIView):
+    @staticmethod
+    def __search(
+            text: Optional[str],
+            filemask: Optional[str],
+            size: Optional[Tuple[int, str]],
+            creation_time: Optional[Tuple[int, str]],
+            search_dir=SEARCH_DIR,
+    ) -> List[str]:
+        """
+        Implements searching files in passed directory.
+        Searches can be performed:
+            - by the occurrence of a substring of text in the content of the file
+            - by file_mask in glob format;
+            - by file size;
+            - by file creation time.
+
+        """
+        pass
+
     def post(self, request) -> Response:
         serializer = SearchSerializer(data=request.data)
         if serializer.is_valid():
